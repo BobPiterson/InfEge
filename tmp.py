29f4388565de2b2caf11432c30f75aee8b4ba1a0
BobPiterson <bob.piterson2022@gmail.com>
2024-01-30 17:23:45 +0300

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,20 +1,17 @@
-def f_left(x,a):
-    return ((x in a) <= (x**2 <= 81))
-def f_right(x,a):
-    return ((x**2 <= 36) <= (x in a))
 
-a = set()
-for i in range(-10, 11):
-    #a.add(i)
-    print(i, end='\t')
-print("")
-# Переберем все значения х и проверим результат функции:
-for x in range(-10, 11):
-     print(f_left(x,a), end='\t')
-print("")
-for x in range(-10, 11):
-     print(f_right(x,a), end='\t')
-print("------------------------------------------------------------------------------------------------------------------------------------------")
-for x in range(-10, 11):
-     print(f_left(x,a) and f_right(x,a), end='\t')
-print("")
+def summa(number):
+    sum_tmp = 0
+    for i in str(number):
+        sum_tmp = sum_tmp + int(i)
+    return sum_tmp
+
+n10 = 2123456783 // 8
+
+n2 = bin(n10)[2:]
+for i in [1,2,3]:
+    if summa(int(n2, 2)) % 2 != 0:
+        n2 = n2 + '1'
+    else:
+        n2 = n2 + '0'
+if int(n2,2) > 987654321 and int(n2,2) < 2123456789:
+    print(n10, int(n2,2))
