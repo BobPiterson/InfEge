f7bc7cadaeb07fa5e85164afc1473d2f981bf682
Siegfried Schmidt <99099240+SiegfriedSchmidt@users.noreply.github.com>
2024-02-01 18:05:30 +0300

    Create div_1_2_B.cpp
    Interesting problem on codeforces

---
+++ olimp/div_1_2_B.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <numeric>
+#include <bitset>
+
+using namespace std;
+
+#define int long long
+
+signed main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int a[n];
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+
+        for (int i = 1; i < 63; ++i) {
+            unsigned int d = (1ull << i);
+            set<int> set1;
+            for (int j = 0; j < n; ++j) {
+                set1.insert(a[j] % d);
+            }
+
+            if (set1.size() == 2) {
+                cout << d << '\n';
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
