b8539a971f25f348f276577fee61f8eb1c997e00
BobPiterson <bob.piterson2022@gmail.com>
2024-02-13 09:47:16 +0300

    Update ITMO-DDoS-ataka.py

--- Olympiada/ITMO-DDoS-ataka.py
+++ Olympiada/ITMO-DDoS-ataka.py
@@ -17,21 +17,21 @@ for i in range(0, 200):
 
 
     # Обработка очереди DDoS
-    if i > 1 and i < 30:
+    if i > 1 and i < 60:
         if i % 2 == 0:
             # Первые 30 запросов каждые 2 мс обрабатываем очередь ddos
             serv.append(ddos[0])
             print('Время = ', i, 'Сервер: Поступил запрос типа ', ddos[0], ', ddos = ', ddos, 'serv = ', serv, 'zapr = ', zapr)
             ddos.remove(ddos[0])
 
-    if i >= 30:
+    if i >= 60:
         if ddos[0] == 1 or ddos[0] == 2:
             serv.append(ddos[0])
             print('Время = ', i, 'Сервер: Поступил запрос типа ', ddos[0], ', ddos = ', ddos, 'serv = ', serv, 'zapr = ', zapr)
             ddos.remove(ddos[0])
 
         if ddos[0] == 0:
-            if (i + 20) % 50 == 0 and i != 30:
+            if (i + 40) % 50 == 0 and i != 60:
                 serv.append(ddos[0])
                 print('Время = ', i, 'Сервер: Поступил 50-й запрос типа ', ddos[0], ', ddos = ', ddos, 'serv = ', serv, 'zapr = ', zapr)
             ddos.remove(ddos[0])
@@ -40,14 +40,14 @@ for i in range(0, 200):
     # Обработка очереди сервера
     # Если сервер ничего не обрабатывает
     if count == 0:
-        if len(serv) > 0: 
+        if len(serv) > 0:
             tip = serv[0]
             if tip == 2:
-                count = 12 + 3
+                count = 12 + 2
             if tip == 1:
-                count = 6 + 3
+                count = 6 + 2
             if tip == 0:
-                count = 3
+                count = 2
             # Удалим запрос из очереди сервера
             serv.remove(tip)
             print('Время = ', i, 'Сервер начал обработку запроса типа ', tip, ' и удалил его из очереди, serv = ', serv)
