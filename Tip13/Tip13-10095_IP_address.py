a63fca7704963a38b4d0834ffbdccf497afa38ad
BobPiterson <bob.piterson2022@gmail.com>
2024-02-14 22:54:38 +0300

    Update Tip13-10095_IP_address.py

--- Tip13/Tip13-10095_IP_address.py
+++ Tip13/Tip13-10095_IP_address.py
@@ -1,6 +1,22 @@
 # https://kompege.ru/variant
 # поразрядная конъюнкция
 # на вход подается IP адрес в формате строки, например "192.168.1.1" и маска в формате строки, например "255.255.255.252"
+def ip2_to_ip10(s_2):
+    tmp = ''
+    s_10 = ''
+    for i in s_2:
+        if i == '.':
+            s_10 = s_10 + str(int(tmp,2)) + '.'
+            tmp = ''
+        else:
+            tmp += i
+    return s_10[:-1]
+
+def dopoln0(s, length):
+    while len(s) < length:
+        s = '0' + s
+    return s
+
 def conjunction(ip, mask):
     ip += '.'
     mask += '.'
@@ -8,9 +24,7 @@ def conjunction(ip, mask):
     tmp = ''
     for j in range(len(ip)):
         if ip[j] == '.':
-            if len(tmp) == 1: tmp = '00' + tmp
-            if len(tmp) == 2: tmp = '0' + tmp
-            ip_oct.append(bin(tmp))
+            ip_oct.append(dopoln0(bin(int(tmp))[2:],8))
             tmp = ''
         else:
             tmp += ip[j]
@@ -19,19 +33,24 @@ def conjunction(ip, mask):
     tmp = ''
     for j in range(len(mask)):
         if mask[j] == '.':
-            if len(tmp) == 1: tmp = '00' + tmp
-            if len(tmp) == 2: tmp = '0' + tmp
-            mask_oct.append(bin(tmp))
+            mask_oct.append(dopoln0(bin(int(tmp))[2:],8))
             tmp = ''
         else:
             tmp += mask[j]
 
-    print(ip_oct, mask_oct)
+    print('IP address host:', ip_oct)
+    print('Mask:           ', mask_oct)
 
-    res_oct = []
+    res_2 = ''
     for i in range(4):
-        for j in range(2, 10):
-            res_oct.append(str(int(ip_oct[i][j]) * int(mask_oct[i][j])))
-    return res_oct
+        for j in range(8):
+            # Побитовое умножение адреса и маски
+            res_2 = res_2 + str(int(ip_oct[i][j]) * int(mask_oct[i][j]))
+        res_2 = res_2 + '.'
+    return res_2
 
-print(conjunction('192.168.10.18', '255.255.255.240'))
+
+s_2 = conjunction('192.168.10.18', '255.255.255.240')
+print('IP address network:', s_2[:-1], ' = ', ip2_to_ip10(s_2))
+
+
