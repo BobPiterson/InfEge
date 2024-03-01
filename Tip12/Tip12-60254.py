aff3a5895b375b7b8b03618af25df37baf091c5a
BobPiterson <bob.piterson2022@gmail.com>
2024-03-01 10:21:29 +0300

    Update Tip12-60254.py

--- Tip12/Tip12-60254.py
+++ Tip12/Tip12-60254.py
@@ -1,24 +1,20 @@
 # https://inf-ege.sdamgia.ru/problem?id=60254
-import time
 def proga(s: str) -> int:
-    time.sleep(10)
-    print('до цикла:', s)
-    while s.find('52') != -1 or s.find('2222') != -1 or s.find('1122') != -1:
-        if s.find('52') != -1:
-            s = s.replace('52', '11')
-        if s.find('2222') != -1:
-            s = s.replace('2222', '5')
-        if s.find('1122') != -1:
-            s = s.replace('1122', '25')
-    print('После цикла', s)
+    while '52' in s or '2222' in s or '1122' in s:
+        if '52' in s:
+            # Согласно условия задачи, меняем ТОЛЬКО ПЕРВОЕ ВХОЖДЕНИЕ СЛЕВА, для этого третьим параметром указываем "1"
+            s = s.replace('52', '11', 1)
+        if '2222' in s:
+            s = s.replace('2222', '5', 1)
+        if '1122' in s:
+            s = s.replace('1122', '25', 1)
     summa = 0
     for i in s:
         summa = summa + int(i)
-    print('summa =', summa)
+    #if summa == 64: print('s =', s, 'summa =', summa)
     return summa
 
-s = '5'
-for n in range(4, 10000):
-    s = s + '2' * n
+for n in range(4, 1000):
+    s = '5' + '2' * n
     if proga(s) == 64:
         print(n)
