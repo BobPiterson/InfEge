6ecd8bcf4a51ae7b5d6b114d83a86a66538ed21b
BobPiterson <bob.piterson2022@gmail.com>
2024-03-07 09:43:39 +0300

    Update MOSH2024-E.py

--- Olympiada/MOSH2024-E.py
+++ Olympiada/MOSH2024-E.py
@@ -1,13 +1,14 @@
 import time
 f = open("C:/Users/vngorlachev/Documents/VisualStudioCode/Projects/txt/e2.txt").read().split()
 n = f[0]
-#print(n)
 text = f[1]
 fano = f[2]
-#print(f[1][-1])
 print(len(text), len(fano))
+abc = 'abcdefghij'
 
 # statistic
+# ------------------------------------------------------------------------------------------------------
+# подсчет повторений каждого символа, в том числе подсчет количества повторений символов, стоящих подряд
 # stat = []
 # includ = set()
 # for c in text:
@@ -19,14 +20,13 @@ print(len(text), len(fano))
 # stat.sort()
 # print(stat)
 
-abc = 'abcdefghij'
-
+# подсчет повторений самых частых пар символов
 # stat = []
 # includ = set()
 # for c in text:
 #     for a1 in abc:
 #         for a2 in abc:
-#             for i in range(18, 30):
+#             for i in range(17, 30):
 #                 tmp = a1 + a2
 #                 if not (tmp in includ):
 #                     if text.count(tmp) == i:
@@ -35,7 +35,7 @@ abc = 'abcdefghij'
 # stat.sort()
 # print(stat)
 
-
+# Подсчет количества повторяющихся кодов в списке кодов фано
 # for i in range(2 ** 12, 2 ** 20):
 #     subs = bin(i)[2:]
 # #    if fano.count(subs * 6) == 1:
@@ -45,42 +45,26 @@ abc = 'abcdefghij'
 #     #     print('h =', subs, fano.count(subs))
 
 # print(text.count('bhe'))
-# print(fano.count(''))
-abc = 'abcdefghij'
-diction = [['a', '.'], ['b', '1010111110'], ['c', '.'], ['d', '1000010101'], ['e', '.'], \
-           ['f', '0010101001'], ['g', '1111111010'], ['h', '0001111110'], ['i', '0001010100'], ['j', '.']]
 
+# найденные соответствия
+diction1 = [['a', '0011010000'], ['b', '1010111110'], ['c', '0100111100'], ['d', '1000010101'], ['e', '1000110010'], \
+           ['f', '0010101001'], ['g', '1111111010'], ['h', '0001111110'], ['i', '0001010100'], ['j', '1010100101']]
 
-fano = fano.replace(diction[8][1], 'i')
-fano = fano.replace(diction[6][1], 'g')
-fano = fano.replace(diction[1][1], 'b')
-    #print(diction[i][1], diction[i][0], fano)
-    #time.sleep(10)
-print(fano)
-
-# subs = ''
-# text2 = ''
-# for s in fano:
-#     subs = subs + s
-#     #print(subs)
-#     #time.sleep(0.01)
-#     for i in range(len(diction)):
-#         #print(diction[i][1])
-#         if diction[i][1] == subs:
-#             print(diction[i][0], end='')
-#             text2 = text2 + diction[i][0]
-#             subs = ''
-#             break
-#         else:
-#             text2 = text2 + ''
-# print()
-# print(text2)
-
-
-# subs = ''
-# for s in fano:
-#     subs = subs + s
-#     print(subs, fano.count(subs))
-#     time.sleep(5)
-#     if fano.count(subs) == 101:
-#         print(subs)
+# Замена кода фано на найденные соответствия и сохранение в список text2
+subs = ''
+text2 = ''
+for s in fano:
+    subs = subs + s
+    for i in range(len(diction1)):
+        if diction1[i][1] == subs:
+            #print(diction[i][0], end='')
+            text2 = text2 + diction1[i][0]
+            subs = ''
+            break
+        else:
+            text2 = text2 + ''
+print()
+print(text2)
+print()
+if text2 == text:
+    print('Все элементы совпадают, таблица найдена: ', diction1)
