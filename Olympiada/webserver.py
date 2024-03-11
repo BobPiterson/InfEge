4dbe3c6275be3a1fb97e858a18036da86763d159
BobPiterson <bob.piterson2022@gmail.com>
2024-03-11 14:57:29 +0300

    Create webserver.py

---
+++ Olympiada/webserver.py
@@ -0,0 +1,6 @@
+import http.server, ssl
+
+server_address = ('172.23.112.127', 8000)
+httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket,server_side=True,certfile='localhost.pem',ssl_version=ssl.PROTOCOL_TLSv1_2)
+httpd.serve_forever()
