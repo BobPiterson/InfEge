29c015e8d6b7b5993e837b3e8fe15e195815b3ea
BobPiterson <bob.piterson2022@gmail.com>
2024-08-30 15:09:55 +0300

    Create Любой_многоугольник.py

---
+++ Программы вводного курса/Любой_многоугольник.py
@@ -0,0 +1,26 @@
+from turtle import *
+delay(10)
+penup()
+goto(-180, -140)
+pendown()
+color('red', 'yellow')
+begin_fill()
+star = 170
+five_star = 144
+triangle = 120
+square = 90
+pentagon = 72
+hexagon = 60
+octagon = 45
+XY = pos()
+max = 360
+left(36)
+for i in range(max):
+    forward(100 * 3)
+    left(five_star)
+    #print(i, '    ', XY[0], XY[1], pos()[0], pos()[1])
+    if XY[0] == round(pos()[0]) and XY[1] == round(pos()[1]):
+        #print('--- Выход по совпадению координат ---')
+        break
+end_fill()
+done()
