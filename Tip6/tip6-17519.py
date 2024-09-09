bfc761f1ed69b3dbfe6496f454d0ba1287c3309b
BobPiterson <bob.piterson2022@gmail.com>
2024-09-09 22:49:11 +0300

    Create tip6-17519.py

---
+++ Tip6/tip6-17519.py
@@ -0,0 +1,32 @@
+from turtle import *
+penup()
+delay(0)
+scale = 10
+#goto(-300, -300)
+#dot(10)
+left(90)
+pendown()
+for i in range(9):
+    forward(22 * scale)
+    right(90)
+    forward(6 * scale)
+    right(90)
+penup()
+dot(10)
+forward(1 * scale)
+right(90)
+forward(5 * scale)
+left(90)
+dot(10)
+
+pendown()
+for i in range(9):
+    forward(53 * scale)
+    right(90)
+    forward(75 * scale)
+    right(90)
+
+
+
+exitonclick()
+print(21+1+21+1)
