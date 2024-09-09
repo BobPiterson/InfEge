d71857ed3bb4515ffe2975ec5cdbe3651487558e
BobPiterson <bob.piterson2022@gmail.com>
2024-09-09 22:49:41 +0300

    Update Любой_многоугольник.py

--- Программы вводного курса/Любой_многоугольник.py
+++ Программы вводного курса/Любой_многоугольник.py
@@ -1,26 +1,37 @@
 from turtle import *
-delay(10)
-penup()
-goto(-180, -140)
-pendown()
-color('red', 'yellow')
-begin_fill()
+# углы различных многоугольников
 star = 170
+star2 = 175
+star3 = 145
 five_star = 144
 triangle = 120
 square = 90
 pentagon = 72
 hexagon = 60
 octagon = 45
+
+delay(0)
+# ширина пера
+width(2)
+# цвет пера и цвет закраски
+color('red', 'yellow')
+penup()
+goto(-60, -250)
+pendown()
+
+begin_fill()
+# сохраним координаты пера в списке
 XY = pos()
+# Зададим максимальное количество повторений цикла
 max = 360
 left(36)
 for i in range(max):
     forward(100 * 3)
-    left(triangle)
-    #print(i, '    ', XY[0], XY[1], pos()[0], pos()[1])
+    # Развернуть против часовой стрелки на указанный угол
+    left(star3)
+    #Если координаты пера повторились, т.е. фигура прорисована, выходим из цикла
     if XY[0] == round(pos()[0]) and XY[1] == round(pos()[1]):
         #print('--- Выход по совпадению координат ---')
         break
 end_fill()
-done()
+exitonclick()
