e7d919dbedd15c223e23b9cefc11fe808b277e7b
BobPiterson <bob.piterson2022@gmail.com>
2024-09-17 17:32:38 +0300

    Update Таблица_ASCII.py

--- Программы вводного курса/Таблица_ASCII.py
+++ Программы вводного курса/Таблица_ASCII.py
@@ -1,11 +1,7 @@
 # Программа выводит соответствие всех печатаемых символов и их ASCII кодов
-# Примечание: с 0 по 31 и 127 коды содержат непечатаемые символы
-# from string import digits, ascii_letters
-# alp = digits + ascii_letters
+# Примечание: с 0 по 31 и 127 коды содержат непечатаемые символы !
 print('Символ', ' ', 'Код')
 print('-------------')
 print('Пробел  ', 32)
-for i in range(33, 127):
+for i in range(33, 256):
     print(chr(i), '      ', i)
-# for c in alp:
-#     print(c, '      ', ord(c))
