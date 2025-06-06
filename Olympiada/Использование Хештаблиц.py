6e2c49323f95f1dcab5eef808098495560d47dad
BobPiterson <bob.piterson2022@gmail.com>
2025-06-06 16:12:19 +0300

    Create Использование Хештаблиц.py

---
+++ Olympiada/Использование Хештаблиц.py
@@ -0,0 +1,16 @@
+# Ассоциативный массив
+internal = []
+
+# Вводим в исходный массив одно значение
+data = {}
+data["key"] = "value"
+#print(data["key"])
+
+for i in range(10):
+    print(abs(hash('key')))
+
+# index = abs(hash('key')) % 1000
+# print(hash('key'))
+# print(abs(hash('key')))
+# print(index)
+
