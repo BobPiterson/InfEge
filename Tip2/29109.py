e14bf775f84e6bc3c41aed175306682b4ddeb25c
BobPiterson <bob.piterson2022@gmail.com>
2025-10-10 15:30:27 +0300

    Update 29109.py

--- Tip2/29109.py
+++ Tip2/29109.py
@@ -1,19 +1,9 @@
-def f(x, y, z, w):
-    return ((z <= w) or (y == w)) and ((x or z) == y)
 
-
+print("x y z w")
 for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                v = int(f(x, y, z, w))
-                if v == 1:
-                    print(x, y, z, w, v, sep='')
+                if (((z <= w) or (y == w)) and ((x or z) == y)) == 1:
+                    print(x, y, z, w)
 
-'''
-
-0110
-*10*
-0**1
-
-'''
