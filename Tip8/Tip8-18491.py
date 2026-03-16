17d1d9366c807120cc18831593880b465f2df2b7
BobPiterson <bob.piterson2022@gmail.com>
2026-03-16 10:31:13 +0300

    Update Tip8-18491.py

--- Tip8/Tip8-18491.py
+++ Tip8/Tip8-18491.py
@@ -9,9 +9,17 @@ for a in 'ОЛГА':
         for c in alf:
             for d in alf:
                 for e in alf:
+<<<<<<< HEAD
                     kod = a+b+c+d+e
                     if kod.count('О') == 1 and kod.count('Л') == 1 and kod.count('Ь') == 1 and kod.count('Г') == 1 and kod.count('А') == 1 and kod.count('ОЬ') == 0 and kod.count('АЬ') == 0:
                         m.append(kod)
 print(m)
 print('Ответ =', len(m))
 # Ответ = 48
+=======
+                    if len({a,b,c,d,e}) == 5 and (a+b+c+d+e).count('ОЬ') == 0 and (a+b+c+d+e).count('АЬ') == 0:
+                        m.append((a+b+c+d+e))
+print(m)
+print('Ответ =', len(m))
+# Ответ = 48
+>>>>>>> 0e8fb460031eca4322e90a46e1b095723d105e7e
