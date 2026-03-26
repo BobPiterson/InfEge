09cc8a605f47f5e7d3dd26e48d598a0f3f219ff4
BobPiterson <bob.piterson2022@gmail.com>
2026-03-26 17:35:20 +0300

    Delete 48389.py

--- Tip14/48389.py
+++
@@ -1,8 +0,0 @@
-mn = 1e9
-for x in range(7):
-    for y in range(7):
-        v = int(f'{y}{x}320', 7) + int(f'1{x}3{y}3', 9)
-        if v % 181 == 0:
-            mn = min(mn, v // 181)
-
-print(mn)
